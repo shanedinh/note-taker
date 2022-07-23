31dfb94c5b380f4323daf58aa15c9b824bef8848
Shane Dinh <shane.dinh5@gmail.com>
2022-07-23 18:23:39 -0500

    server up and running

---
+++ server.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const notesdb = require('./db/db.json');
+const res = require('express/lib/response');
+
+const PORT = process.env.PORT || 3003;
+const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+// make publice files readilly available
+app.use(express.static('public'));
+
+// POST routes
+app.post('/api/notes', (req, res) => {
+    console.log('/api/notes post was hit');
+    let note = req.body;
+    let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+
+    note.id = note.length;
+    noteList.push(note);
+    fs.writeFileSync('./db/db.json', JSON.stringify(noteList));
+    res.json(noteList);
+});
+
+// GET routes
+// return the index file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+// return the notes html file
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+// return the db.json file
+app.get('/api/notes', (req, res) => {
+    return res.json(notesdb);
+});
+
+// return index html for any other urls
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// delete route
+app.delete('/api/notes/:id', (req, res) => {
+    let noteId = req.params.id;
+    let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+
+    noteList = noteList.filter(note => {
+        return note.id != noteId;
+    })
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(noteList));
+    res.json(noteList);
+});
+
+
+app.listen(PORT, () => {
+    console.log(`listening at http://localhost:${PORT}`);
+});
